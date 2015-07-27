a8e63bc3bbba94e33bf53ed807b691f619fbcf2a
Jean-Christophe Sirot <jcsirot@gmail.com>
2015-07-27 10:39:52 +0200

    Refactor tests

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,16 @@
 extern crate nom;
 extern crate regex;
 
-use nom::{IResult, space, digit, alphanumeric, multispace, is_alphanumeric, is_space};
+use nom::{IResult, space, digit, multispace, is_alphanumeric, is_space};
 use nom::IResult::*;
 use nom::Err::*;
 use regex::{Regex};
 use std::str;
+
+#[derive(PartialEq,Eq,Debug)]
+pub struct PGNGame<'a> {
+    moves: Vec<HalfMove<'a>>
+}
 
 #[derive(PartialEq,Eq,Debug)]
 pub enum Piece {
@@ -29,13 +34,16 @@ pub enum HalfMove<'a> {
         promotion: Option<Piece>
     },
     KingsideCastling,
-    QueensideCastling
+    QueensideCastling,
+    None
 }
 
 
 #[derive(PartialEq,Eq,Debug)]
 pub struct Move<'a> {
-    id: &'a str
+    id: &'a str,
+    white: HalfMove<'a>,
+    black: HalfMove<'a>
 }
 
 fn piece_parser(p: &str) -> Piece {
@@ -45,7 +53,7 @@ fn piece_parser(p: &str) -> Piece {
         "B" => Piece::Bishop,
         "N" => Piece::Knight,
         "R" => Piece::Rook,
-        _ => panic!("Impossible")
+        _ => unreachable!()
     }
 }
 
@@ -104,7 +112,7 @@ fn pawn_capture(input:&[u8]) -> IResult<&[u8], HalfMove> {
 
 fn piece_move(input:&[u8]) -> IResult<&[u8], HalfMove > {
     let mut index = 0;
-    let re = Regex::new(r"^([KQBNR])([a-h])?([1-8])?([a-h][1-8])$").unwrap();
+    let re = Regex::new(r"^([KQBNR])([a-h])?([1-8])?(x)?([a-h][1-8])$").unwrap();
 
     for idx in 0..input.len() {
         if is_space(input[idx]) {
@@ -120,52 +128,24 @@ fn piece_move(input:&[u8]) -> IResult<&[u8], HalfMove > {
         return Done(&input[index..],
                     HalfMove::Regular {
                         piece: piece_parser(cap.at(1).unwrap()),
-                        capture: false,
+                        capture: cap.at(4).is_some(),
                         col_from: cap.at(2),
                         row_from: cap.at(3),
-                        square: cap.at(4).unwrap(),
+                        square: cap.at(5).unwrap(),
                         promotion: None
                     });
     }
     return Error(Position(0, input));
 }
 
-fn piece_capture(input:&[u8]) -> IResult<&[u8], HalfMove > {
-    let mut index = 0;
-    let re = Regex::new(r"^([KQBNR])([a-h])?([1-8])?x([a-h][1-8])$").unwrap();
-
-    for idx in 0..input.len() {
-        if is_space(input[idx]) {
-            break;
-        }
-        index = index + 1;
-    }
-
-    let text = str::from_utf8(&input[..index]).unwrap_or("");
-
-    if re.is_match(text) {
-        let cap = re.captures(text).unwrap();
-        return Done(&input[index..],
-                    HalfMove::Regular {
-                        piece: piece_parser(cap.at(1).unwrap()),
-                        capture: true,
-                        col_from: cap.at(2),
-                        row_from: cap.at(3),
-                        square: cap.at(4).unwrap(),
-                        promotion: None
-                    });
-    }
-    return Error(Position(0, input));
-}
-
-named!(san_move<&[u8], HalfMove>,
+named!(san<&[u8], HalfMove>,
        alt!(
            pawn_move |
            pawn_capture |
            piece_move |
-           piece_capture
+           tag!("O-O-O") =>  { |_| return HalfMove::QueensideCastling } |
+           tag!("O-O") => { |_| return HalfMove::KingsideCastling }
        ));
-
 
 fn alphanumeric_or_underscore(input:&[u8]) -> IResult<&[u8], &[u8]> {
     for idx in 0..input.len() {
@@ -198,91 +178,38 @@ named!(mve<&[u8], Move>,
            i: map_res!(digit, str::from_utf8) ~
            tag!(".") ~
            multispace? ~
-           san_move ~
+           white: san ~
            space ~
            multispace? ~
-           san_move ~
-           multispace?,
-       || {Move { id: i } } ));
+           black: san? ~
+           multispace? ,
+       || { Move { id: i,
+                  white: white,
+                  black: black.unwrap_or(HalfMove::None) }}
+        ));
+
+named!(moves<&[u8], Move>,
+       chain!(
+           i: map_res!(digit, str::from_utf8) ~
+           tag!(".") ~
+           multispace? ~
+           white: san ~
+           space ~
+           multispace? ~
+           black: san? ~
+           multispace? ,
+       || { Move { id: i,
+                  white: white,
+                  black: black.unwrap_or(HalfMove::None) }}
+        ));
 
 fn main() {
-
-    assert_eq!(mve(b"7.e4 e5"), Done(&b""[..], Move{id: "7"}));
-
-    let m3 = HalfMove::Regular {
-        piece: Piece::Queen,
-        capture: false,
-        col_from: None,
-        row_from: None,
-        square: "f2",
-        promotion: None };
-    assert_eq!(piece_move(b"Qf2"), Done(&b""[..], m3));
-
-    let m4 = HalfMove::Regular {
-        piece: Piece::Bishop,
-        capture: false,
-        col_from: Some("g"),
-        row_from: None,
-        square: "a8",
-        promotion: None };
-    assert_eq!(piece_move(b"Bga8"), Done(&b""[..], m4));
-
-    let m5 = HalfMove::Regular {
-        piece: Piece::Rook,
-        capture: false,
-        col_from: None,
-        row_from: Some("1"),
-        square: "e1",
-        promotion: None };
-    assert_eq!(piece_move(b"R1e1"), Done(&b""[..], m5));
-
-    let m6 = HalfMove::Regular {
-        piece: Piece::Rook,
-        capture: false,
-        col_from: Some("c"),
-        row_from: Some("4"),
-        square: "f4",
-        promotion: None };
-    assert_eq!(piece_move(b"Rc4f4"), Done(&b""[..], m6));
-
-    let m7 = HalfMove::Regular {
-        piece: Piece::King,
-        capture: true,
-        col_from: None,
-        row_from: None,
-        square: "f2",
-        promotion: None };
-    assert_eq!(piece_capture(b"Kxf2"), Done(&b""[..], m7));
-
-    let m8 = HalfMove::Regular {
-        piece: Piece::Queen,
-        capture: true,
-        col_from: Some("h"),
-        row_from: None,
-        square: "b5",
-        promotion: None };
-    assert_eq!(piece_capture(b"Qhxb5"), Done(&b""[..], m8));
-
-    let m100 = HalfMove::Regular {
-        piece: Piece::Pawn,
-        capture: false,
-        col_from: None,
-        row_from: None,
-        square: "e3",
-        promotion: None };
-    assert_eq!(san_move(b"e3 f5"), Done(&b" f5"[..], m100));
-
-    /*
-    assert_eq!(hmve(b"Rd7"), Done(&b"d7"[..], HalfMove::Regular{piece: Piece::Rook, capture: false}));
-    assert_eq!(hmve(b"g5"), Done(&b"g5"[..], HalfMove::Regular{piece: Piece::Pawn, capture: false}));
-    assert_eq!(hmve(b"Bxf2"), Done(&b"f2"[..], HalfMove::Regular{piece: Piece::Bishop, capture: true}));
-     */
 }
 
 #[cfg(test)]
 mod tests {
-    use super::{headers, alphanumeric_or_underscore, pawn_move, pawn_capture};
-    use super::{Piece, HalfMove};
+    use super::{headers, alphanumeric_or_underscore, pawn_move, pawn_capture, piece_move, mve};
+    use super::{Piece, HalfMove, Move};
     use nom::IResult::*;
 
     #[test]
@@ -362,5 +289,117 @@ mod tests {
             square: "b8",
             promotion: Some(Piece::Rook) };
         assert_eq!(pawn_capture(b"axb8=R"), Done(&b""[..], m2));
+    }
+
+    #[test]
+    fn piece_move_tests() {
+        let m1 = HalfMove::Regular {
+            piece: Piece::Queen,
+            capture: false,
+            col_from: None,
+            row_from: None,
+            square: "f2",
+            promotion: None };
+        assert_eq!(piece_move(b"Qf2"), Done(&b""[..], m1));
+
+        let m2 = HalfMove::Regular {
+            piece: Piece::Queen,
+            capture: true,
+            col_from: None,
+            row_from: None,
+            square: "f2",
+            promotion: None };
+        assert_eq!(piece_move(b"Qxf2"), Done(&b""[..], m2));
+
+        let m3 = HalfMove::Regular {
+            piece: Piece::Bishop,
+            capture: false,
+            col_from: Some("g"),
+            row_from: None,
+            square: "a8",
+            promotion: None };
+        assert_eq!(piece_move(b"Bga8"), Done(&b""[..], m3));
+
+        let m4 = HalfMove::Regular {
+            piece: Piece::Bishop,
+            capture: true,
+            col_from: Some("g"),
+            row_from: None,
+            square: "a8",
+            promotion: None };
+        assert_eq!(piece_move(b"Bgxa8"), Done(&b""[..], m4));
+
+        let m5 = HalfMove::Regular {
+            piece: Piece::Rook,
+            capture: false,
+            col_from: None,
+            row_from: Some("1"),
+            square: "e1",
+            promotion: None };
+        assert_eq!(piece_move(b"R1e1"), Done(&b""[..], m5));
+
+        let m6 = HalfMove::Regular {
+            piece: Piece::Rook,
+            capture: true,
+            col_from: None,
+            row_from: Some("1"),
+            square: "e1",
+            promotion: None };
+        assert_eq!(piece_move(b"R1xe1"), Done(&b""[..], m6));
+
+        let m7 = HalfMove::Regular {
+            piece: Piece::Rook,
+            capture: false,
+            col_from: Some("c"),
+            row_from: Some("4"),
+            square: "f4",
+            promotion: None };
+        assert_eq!(piece_move(b"Rc4f4"), Done(&b""[..], m7));
+
+        let m8 = HalfMove::Regular {
+            piece: Piece::Rook,
+            capture: true,
+            col_from: Some("c"),
+            row_from: Some("4"),
+            square: "f4",
+            promotion: None };
+        assert_eq!(piece_move(b"Rc4xf4"), Done(&b""[..], m8));
+    }
+
+    #[test]
+    fn move_tests() {
+        let move_white = HalfMove::Regular {
+            piece: Piece::Pawn,
+            capture: false,
+            col_from: None,
+            row_from: None,
+            square: "e4",
+            promotion: None };
+        let move_black = HalfMove::Regular {
+            piece: Piece::Pawn,
+            capture: false,
+            col_from: None,
+            row_from: None,
+            square: "e5",
+            promotion: None };
+        assert_eq!(mve(b"7.e4 e5"), Done(&b""[..], Move {id: "7", white: move_white, black: move_black}));
+
+        let move_white_2 = HalfMove::Regular {
+            piece: Piece::Bishop,
+            capture: false,
+            col_from: None,
+            row_from: None,
+            square: "f2",
+            promotion: None };
+        assert_eq!(mve(b"12.Bf2 O-O"), Done(&b""[..], Move {id: "12", white: move_white_2, black: HalfMove::KingsideCastling}));
+
+        let move_black_2 = HalfMove::Regular {
+            piece: Piece::Pawn,
+            capture: true,
+            col_from: Some("g"),
+            row_from: None,
+            square: "h4",
+            promotion: None };
+        assert_eq!(mve(b"4.O-O-O gxh4"), Done(&b""[..], Move {id: "4", white: HalfMove::QueensideCastling, black: move_black_2}));
     }
 }
