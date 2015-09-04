6a8a9e65030015424dda0936ac9deb5807a1dcf2
Jean-Christophe Sirot <jcsirot@gmail.com>
2015-09-04 10:54:57 +0200

    Remove main function. Code moved to examples

--- src/lib.rs
+++ src/lib.rs
@@ -268,66 +268,6 @@ named!(game<&[u8], PGNGame>,
        )
 );
 
-fn main() {
-
-    let game2 = "[Event \"F/S Return Match\"]
-[Site \"Belgrade, Serbia JUG\"]
-[Date \"1992.11.04\"]
-[Round \"29\"]
-[White \"Fischer, Robert J.\"]
-[Black \"Spassky, Boris V.\"]
-[Result \"1/2-1/2\"]
-
-1. e4 e5 2. Nf3 Nc6 3. Bb5 a6 4. Ba4 Nf6 5. O-O Be7 6. Re1 b5 7. Bb3 d6
-8. c3 O-O 9. h3 Nb8 10. d4 Nbd7 11. c4 c6 12. cxb5 axb5 13. Nc3 Bb7 14. Bg5 b4 15.
-Nb1 h6 16. Bh4 c5 17. dxe5 Nxe4 18. Bxe7 Qxe7 19. exd6 Qf6 20. Nbd2 Nxd6 21.
-Nc4 Nxc4 22. Bxc4 Nb6 23. Ne5 Rae8 24. Bxf7+ Rxf7 25. Nxf7 Rxe1+ 26. Qxe1 Kxf7
-27. Qe3 Qg5 28. Qxg5 hxg5 29. b3 Ke6 30. a3 Kd6 31. axb4 cxb4 32. Ra5 Nd5 33.
-f3 Bc8 34. Kf2 Bf5 35. Ra7 g6 36. Ra6+ Kc5 37. Ke1 Nf4 38. g3 Nxh3 39. Kd2 Kb5
-40. Rd6 Kc5 41. Ra6 Nf2 42. g4 Bd3 43. Re6 1/2-1/2";
-
-    let g = PGNGame::from_str(game2);
-    match g {
-        Ok(game) =>  {
-
-            fn print_half_move<'a>(p: &'a str, capture: bool, row_from: Option<&'a str>, col_from: Option<&'a str>, square: &'a str, promotion: Option<&'a str>) {
-                let cap = if capture {"x"} else {""};
-                let from = format!("{}{}", row_from.unwrap_or(""), col_from.unwrap_or(""));
-                let prom = promotion.map(|p| format!("={}", p)).unwrap_or(String::from(""));
-                print!("{}{}{}{}{}", p, from, cap, square, prom);
-            }
-
-            for (k, v) in game.headers.iter() {
-                println!("{}: {}", k, v);
-            }
-            for (i, m) in game.moves.iter().cloned().enumerate() {
-                let is_white_turn = i % 2 == 0;
-                if is_white_turn {
-                    print!("{}. ", i/2 + 1);
-                }
-                match m {
-                    HalfMove::Regular{piece, capture, col_from, row_from, square, promotion} => {
-                        let p = if is_white_turn { piece.as_white() } else { piece.as_black() };
-                        let prom = if is_white_turn { promotion.as_ref().map(Piece::as_white) } else { promotion.as_ref().map(Piece::as_black) };
-                        print_half_move(p, capture,row_from, col_from, square, prom);
-                    },
-                    HalfMove::KingsideCastling => print!("O-O"),
-                    HalfMove::QueensideCastling => print!("O-O-O"),
-                    _ => unreachable!()
-                }
-                if is_white_turn {
-                    print!("\t");
-                } else {
-                    print!("\n");
-                }
-            }
-        },
-        Err(x) => panic!(x)
-    }
-
-}
-
-
 #[cfg(test)]
 mod tests {
     use super::{headers, alphanumeric_or_underscore, pawn_move, pawn_capture, piece_move, mve, moves};
